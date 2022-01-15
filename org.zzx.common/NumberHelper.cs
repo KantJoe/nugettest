5c25f4ed0ab1da109404f46f3debc8cb7fbdfa93
kant <zi2xing@126.com>
2022-01-15 18:29:52 +0800

    commit

---
+++ org.zzx.common/NumberHelper.cs
@@ -0,0 +1,11 @@
+﻿namespace org.zzx.common
+{
+    public static class NumberHelper
+    {
+
+        public static int Add(this int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
